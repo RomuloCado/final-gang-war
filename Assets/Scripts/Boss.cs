4bb5796352a601e256315d0c59eacaefdb22ae67
romulo <romulo.dorneles@compasso.com.br>
2023-06-27 21:45:57 -0300

    unity project

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boss : Enemy {
+
+	public GameObject boomerang;
+	public float minBoomerangTime, maxBoomerangTime;
+
+	private MusicController music;
+
+	// Use this for initialization
+	void Awake () {
+        isBoss = true;
+        Invoke("ThrowBoomerang", Random.Range(minBoomerangTime, maxBoomerangTime));
+		music = FindObjectOfType<MusicController>();
+		music.PlaySong(music.bossSong);
+	}
+	
+	void ThrowBoomerang()
+	{
+		if (!isDead)
+		{
+			anim.SetTrigger("Boomerang");
+			GameObject tempBoomerang = Instantiate(boomerang, transform.position, transform.rotation);
+			if (facingRight)
+			{
+				tempBoomerang.GetComponent<Boomerang>().direction = 1;
+			}
+			else
+			{
+				tempBoomerang.GetComponent<Boomerang>().direction = -1;
+			}
+			Invoke("ThrowBoomerang", Random.Range(minBoomerangTime, maxBoomerangTime));
+		}
+	}
+
+	void BossDefeated()
+	{
+		music.PlaySong(music.levelClearSong);
+		FindObjectOfType<UIManager>().UpdateDisplayMessage("Level Clear");
+		Invoke("LoadScene", 8f);
+	}
+
+	void LoadScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+	public AudioClip levelSong, bossSong, levelClearSong;
+
+	private AudioSource audioS;
+
+	// Use this for initialization
+	void Start () {
+
+		audioS = GetComponent<AudioSource>();
+		PlaySong(levelSong);
+
+	}
+	
+	
+
+	public void PlaySong(AudioClip clip)
+	{
+		audioS.clip = clip;
+		audioS.Play();
+	}
+}
